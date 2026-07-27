6136eda498fc99821f736860ff2f6fa63f8fb139
karatoauto-design <karato.auto@gmail.com>
2026-07-28 01:27:25 +0700

    Publish SKBB Practice

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE = 'skbb-practice-v1';
+const FILES = ['./', './index.html', './manifest.webmanifest'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
+self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(res => { const copy = res.clone(); caches.open(CACHE).then(c => c.put(e.request, copy)); return res; }))));
